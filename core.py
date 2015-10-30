fd51dc78611aaf68a4c1a1d7a1f3f99e38a9fa52
Ryan Freckleton <ryan.freckleton@gmail.com>
2015-10-30 13:49:48 -0600

    Adding documentation and more tests

--- core.py
+++ core.py
@@ -1,9 +1,24 @@
 def get_threads(comment):
+    """
+    Given a root comment, return a list of the 'branches' or 'threads of
+    discussion' for it. For example:
+
+        comment 1
+            commetn 2
+            comment 3
+                comment 4
+
+    will return:
+
+         [comment 1, comment 2]
+         [comment 1, comment 3, comment 4]
+    """
+
     if not comment.replies:
-        yield comment,
+        yield [comment]
     for reply in comment.replies:
         if not reply.replies:
-            yield comment, reply
+            yield [comment, reply]
         else:
             for r in get_threads(reply):
-                yield (comment,) + r
+                yield [comment] + r
--- spec_core.py
+++ spec_core.py
@@ -22,21 +22,28 @@ class Comment(object):
 class DescribeGetPairs:
     def should_get_simple_thread(self):
         comment1 = Comment('Comment 1')
-        assert [(comment1,)] == list(get_threads(comment1))
+        assert [[comment1]] == list(get_threads(comment1))
 
     def should_get_two_thread(self):
         comment2 = Comment('Comment 2')
         comment1 = Comment('Comment 1', replies=[comment2])
-        assert [(comment1, comment2)] == list(get_threads(comment1))
+        assert [[comment1, comment2]] == list(get_threads(comment1))
 
     def should_get_multiple_children(self):
         comment3 = Comment('Comment 3')
         comment2 = Comment('Comment 2')
         comment1 = Comment('Comment 1', replies=[comment2, comment3])
-        assert [(comment1, comment2), (comment1, comment3)] == list(get_threads(comment1))
+        assert [[comment1, comment2], [comment1, comment3]] == list(get_threads(comment1))
 
     def should_get_three_thread(self):
         comment3 = Comment('Comment 3')
         comment2 = Comment('Comment 2', replies=[comment3])
         comment1 = Comment('Comment 1', replies=[comment2])
-        assert [(comment1, comment2, comment3)] == list(get_threads(comment1))
+        assert [[comment1, comment2, comment3]] == list(get_threads(comment1))
+
+    def should_get_two_threads_different_length(self):
+        comment4 = Comment('Comment 4')
+        comment3 = Comment('Comment 3', replies=[comment4])
+        comment2 = Comment('Comment 2')
+        comment1 = Comment('Comment 1', replies=[comment2, comment3])
+        assert [[comment1, comment2], [comment1, comment3, comment4]] == list(get_threads(comment1))
